10db2ecb8c73a1ea641908c05ed42c5c53044d87
17111057 <17111057@w3332152.portoalegre.pucrsnet.br>
2018-09-13 20:31:10 -0300

    Metodo quantidade de socios

--- PadroesDeProjeto/src/Bar.java
+++ PadroesDeProjeto/src/Bar.java
@@ -11,16 +11,27 @@ public class Bar {
 	}
 
 	// pesquisa por CPF
-	public Cliente pesquisaPorCpf(String cpf) {
+	public boolean pesquisaPorCpf(String cpf) {
 		for (Cliente c : listaClientes) {
 			if (c.getCpf().equals(cpf)) {
-				return c;
+				return true;
 			}
 		}
-		return null;
+		return false;
 	}
 	// quantidade de pessoas
 	
 	// distribuição por genero(percentual de clientes masculino e feminino)
+	
 	// status de sócio (quantos são sócios, e quantos não são).
+	
+	public int qntSocios() {
+		int socio =0;
+		for (Cliente c : listaClientes) {
+			if(c instanceof Socio) {
+				socio++;
+			}	
+		}
+		return socio;
+	}
 }
